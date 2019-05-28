df74f04e6736149522f3cb2d990b7f42a1fbad8a
Kacper Matuszek <kacpermatuszek2@gmail.com>
2019-05-28 17:29:43 +0200

    Added rest of images, Helpers for images. Models menu is completed.

---
+++ AudiShop/AudiShop/Helpers/AppConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace AudiShop.Helpers
+{
+    public class AppConfig
+    {
+        private static readonly string _modelIcons = ConfigurationManager.AppSettings["ModelIcons"];
+
+        public static string ModelIcons => _modelIcons;
+    }
+}
---
+++ AudiShop/AudiShop/Helpers/UrlHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AudiShop.Helpers
+{
+    public static class UrlHelpers
+    {
+        public static string PathOfModel(this UrlHelper param, string modelName)
+        {
+            var _modelIcons = AppConfig.ModelIcons;
+            var _path = Path.Combine(_modelIcons, modelName);
+            var _absolutePath = param.Content(string.Format("{0}.jpg",_path));
+
+            return _absolutePath;
+        }
+    }
+}
