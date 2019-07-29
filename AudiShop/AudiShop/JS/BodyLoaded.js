4bff5b30fc3da40f90f83f5eb0d4b0bc1c5d201b
Kacper <kacpermatuszek2@gmail.com>
2019-07-29 23:17:16 +0200

    Added animation to home website.

--- AudiShop/AudiShop/JS/BodyLoaded.js
+++ AudiShop/AudiShop/JS/BodyLoaded.js
@@ -6,3 +6,23 @@ function SetHeightToAnotherEl()
                 
     document.getElementsByClassName('Wallpaper')[0].style.height = _heightToAnotherEl + 'px';
 }
+
+function expandMenu() {
+    var element = document.getElementsByClassName("arrow")[0];
+
+    if (element.classList.contains("fa-arrow-down")) {
+        element.classList.remove("fa-arrow-down");
+        element.classList.add("fa-arrow-up");
+        return;
+    }
+
+    element.classList.remove("fa-arrow-up");
+    element.classList.add("fa-arrow-down");
+}
+
+$(document).ready(function () {
+    $(".arrow").click(function () {
+        $(".VerticalContent").toggleClass("slide-out");
+        $(".VerticalContent").toggleClass("slide-in");
+    });
+});
